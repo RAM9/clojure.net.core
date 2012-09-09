4e3a52ee63a85b934771bb69e42df9a43c541e7e
Hollin Wilkins <chromaticbum@gmail.com>
2012-09-09 16:22:40 -0700

    Sending join message.

--- src/clojure/net/cmd.clj
+++ src/clojure/net/cmd.clj
@@ -2,27 +2,26 @@
   (:use [gloss core io]))
 
 (defcodec id (finite-frame :int16
-                                (string :utf-8)))
+                           (string :utf-8)))
 (defcodec host (finite-frame :int16
                              (string :utf-8)))
 (defcodec port :int32)
 
-(defcodec node-info {:id id
-                :host host
-                :port port})
+(defcodec ninfo {:host host
+                 :port port})
 
-(defcodec node-infos (finite-frame
-                       :int32
-                       (repeated node-info)))
+(defcodec ninfos (finite-frame
+                   :int32
+                   (repeated ninfo)))
 
 (defcodec ctype (enum :byte :join :ok :ping :error))
 
 (defcodec join {:type :join
-                :node-info node-info})
+                :ninfo ninfo})
 
 (defcodec ok {:type :ok
-              :node-info node-info
-              :remote-nodes node-infos})
+              :ninfo ninfo
+              :remote-nodes ninfos})
 
 (defcodec ping {:type :ping})
 
--- src/clojure/net/core.clj
+++ src/clojure/net/core.clj
@@ -5,37 +5,74 @@
   (:use [lamina.core]
         [clojure.tools.logging :only (debug info warn)]))
 
-(defrecord ServerNode [ninfo conn rnodes open-conns])
-(defrecord NodeInfo [id host port])
-
-(defn ninfo [id host port]
-  (NodeInfo.
-    id
-    host
-    port))
+(defn ninfo [host port]
+   {:host host
+   :port port})
 
 (defn snode [ninfo]
-  (ServerNode.
-    ninfo
-    nil
-    #{}
-    #{}))
+  {:ninfo ninfo
+   :conn nil
+   :rnodes #{}})
 
 (defn snode! [ninfo]
   (agent (snode ninfo)))
 
+(defn add-node [{:keys [rnodes] :as server} conn {:keys [ninfo] :as msg}])
+
+(defn handle-join [server! conn]
+  (run-pipeline
+    conn
+    read-channel
+    (fn [msg]
+      (debug "receive" msg)
+      (if (= (:type msg) :join)
+        (do)
+        ;(send-off server! add-node conn msg)
+        (throw (Exception. "first message was not join!"))))))
+
 (defn server-handler [server!]
-  (fn [result-conn client-info]))
+  (fn [result-conn client-info]
+    (run-pipeline
+      result-conn
+      (fn [conn] (handle-join server! conn)))))
 
 (defn start [ninfo]
   (info "starting server" ninfo)
   (let [server! (snode! ninfo)
         conn (tcp/start-tcp-server (server-handler server!) {:port (:port ninfo)
-                                                              :frame cmd/frame})]
+                                                             :frame cmd/frame})]
     (send-off server! assoc :conn conn)
     server!))
+
+(defn sjoin2 [server! conn]
+  (enqueue conn {:type :join
+                 :ninfo (:ninfo @server!)}))
+
+(defn sjoin [server! host port]
+  (let [result-conn (tcp/tcp-client
+               {:host host
+                :port port
+                :frame cmd/frame})]
+    (run-pipeline
+      result-conn
+      #(sjoin2 server! %))))
+
+(defn run-test []
+  (def serv1 (start (ninfo "localhost" 6661)))
+  (def serv2 (start (ninfo "localhost" 6662)))
+  ;(def serv3 (start (ninfo "node3" "localhost" 6663)))
+  ;(def serv4 (start (ninfo "node4" "localhost" 6664)))
+  ;(def serv5 (start (ninfo "node5" "localhost" 6665)))
+
+  (sjoin serv1 "localhost" 6662))
+
+;(cjoin! (:server! serv2) "localhost" 6661)
+;(cjoin! (:server! serv3) "localhost" 6661)
+;(cjoin! (:server! serv4) "localhost" 6661)
+;(cjoin! (:server! serv5) "localhost" 6661))
 
 (defn -main
   "I don't do a whole lot."
   [& args]
+  (run-test)
   (println "Hello, World!"))
